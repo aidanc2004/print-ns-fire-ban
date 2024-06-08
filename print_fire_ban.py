139f903e19686eae011a01462e84e68161ae9358
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2024-06-08 19:23:42 -0300

    Crop img to A4 150dpi size

--- print_fire_ban.py
+++ print_fire_ban.py
@@ -5,6 +5,7 @@
 
 import os
 import time
+import math
 import win32api
 import win32print
 import schedule
@@ -12,8 +13,35 @@ from selenium import webdriver
 from PIL import Image
 from io import BytesIO
 
+# Set the Firefox window size to get all content on the page
+def set_window_size(driver):
+  window_width = driver.execute_script("return Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );")
+  window_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+  driver.set_window_size(window_width, window_height)
+
+# Crop image to only needed information  
+def crop_img(driver, img, size):
+  # Section to get, from legend (included) to the county after Annapolis (not included)
+  table_legend = driver.find_element("class name", "table-legend")
+  antigonish_county = driver.find_element("id", "Antigonish-County")
+  
+  table_location = table_legend.location
+  county_location = antigonish_county.location
+
+  # Location of needed info
+  left = table_location["x"] - 30
+  top = table_location["y"]
+  right = table_location["x"] + size["width"]
+  bottom = county_location["y"]
+  
+  img = img.crop((left, top, right, bottom))
+
+  #return (img, (new_width, new_height))
+  return img
+
 def fireban_png(file):
   url = "https://novascotia.ca/burnsafe/"
+  
   # Start Firefox in headless mode
   options = webdriver.firefox.options.Options()
   options.add_argument("--headless")
@@ -22,35 +50,28 @@ def fireban_png(file):
   # Open firefox and set window width/height
   driver.get(url)
   page = driver.find_element("id", "burn-data-content")
-  size = page.size
-
-  window_width = driver.execute_script("return Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );")
-  window_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
-  driver.set_window_size(window_width, window_height)
+  set_window_size(driver)
   
   # Get content as a png
   png = driver.get_screenshot_as_png()
-  
-  table_legend = driver.find_element("class name", "table-legend")
-  # Not showing Antigonish County
-  antigonish_county = driver.find_element("id", "Antigonish-County")
-  
-  table_location = table_legend.location
-  county_location = antigonish_county.location
 
-  # Crop then save to file  
+  # Crop PNG to only needed information
   img = Image.open(BytesIO(png))
+  img = crop_img(driver, img, page.size)
 
-  left = table_location["x"] - 30
-  top = table_location["y"]
-  right = table_location["x"] + size["width"]
-  bottom = county_location["y"]
-  img = img.crop((left, top, right, bottom))
+  # New image for printer paper (A4 at 150dpi)
+  paper = Image.new(mode="RGB", size=(1240, 1754), color=(255, 255, 255))
+
+  # Paste image into the middle of the paper
+  img_width, img_height = img.size
+  new_width = 1240
+  new_height = math.floor(new_width * img_width / img_height)
+
+  img = img.resize((new_width, new_height), Image.LANCZOS)
+  paper.paste(img, (0, (math.floor(1754/2)) - (math.floor(new_height/2))))
   
-  img = img.resize((595, 842)) # A4 at 72dpi
-  
-  #img.show()
-  img.save(file)
+  #paper.show()
+  paper.save(file)
   
   # Exit Firefox
   driver.quit()
