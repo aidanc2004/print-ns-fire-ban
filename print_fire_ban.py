6e306e4faa73bada5c4d21246e5d6d00d1ab59a2
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2024-06-10 11:20:08 -0300

    Error checking for getting and printing fire ban

--- print_fire_ban.py
+++ print_fire_ban.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Download the current nova scotia fire ban at 2pm everyday
-# Orchard Queen Motel and RV Park, Aidan Carey, June 8th 2024
+# Aidan Carey, June 8th 2024
 
 import os
 import time
@@ -36,7 +36,6 @@ def crop_img(driver, img, size):
   
   img = img.crop((left, top, right, bottom))
 
-  #return (img, (new_width, new_height))
   return img
 
 def fireban_png(file):
@@ -47,18 +46,29 @@ def fireban_png(file):
   options.add_argument("--headless")
   driver = webdriver.Firefox(options=options)
   
-  # Open firefox and set window width/height
-  driver.get(url)
-  page = driver.find_element("id", "burn-data-content")
-  set_window_size(driver)
-  
-  # Get content as a png
-  png = driver.get_screenshot_as_png()
+  # Open website
+  try:
+    driver.get(url)
+  except:
+    print(f"Couldn't get {url}")
+    return -1
 
-  # Crop PNG to only needed information
+  # Page might not have loaded fast enough
+  try:
+    page = driver.find_element("id", "burn-data-content")
+  except:
+    print("Couldn't find burn-data-content")
+    return -1
+  
+  set_window_size(driver) # Set Firefox size
+
+  # Get PNG of website and crop to only needed information
+  png = driver.get_screenshot_as_png()
   img = Image.open(BytesIO(png))
   img = crop_img(driver, img, page.size)
 
+  driver.quit() # Exit Firefox
+  
   # New image for printer paper (A4 at 150dpi)
   paper = Image.new(mode="RGB", size=(1240, 1754), color=(255, 255, 255))
 
@@ -70,29 +80,30 @@ def fireban_png(file):
   img = img.resize((new_width, new_height), Image.LANCZOS)
   paper.paste(img, (0, (math.floor(1754/2)) - (math.floor(new_height/2))))
   
-  #paper.show()
   paper.save(file)
-  
-  # Exit Firefox
-  driver.quit()
+
+  return 0
 
 # Get the fire ban and print it to the default printer
 def print_fireban():
   # Save NS fire ban website as a png
   file = "fireban.png"
-  fireban_png(file)
+  err = fireban_png(file)
+  if (err < 0):
+    return
   print(f"Saved to {file}")
   
   # Print PDF to printer (Windows specific)
   printer_name = "Brother MFC-L2740DW series Printer"
-  win32api.ShellExecute(0, "printto", file, f'"{printer_name}"', ".", 0)
+  err = win32api.ShellExecute(0, "printto", file, f'"{printer_name}"', ".", 0)
+  if (err <= 32):
+    print(f"Failed to print to f{printer_name}")
+    return
   print(f"Printing to {printer_name}")
 
-# Run print_fireban() everyday at 2pm
-print_fireban() # Test
-"""
+# Run print_fireban everyday at 2pm
 schedule.every().day.at("14:00").do(print_fireban)
 while True:
   schedule.run_pending()
   time.sleep(60)
-"""
+
